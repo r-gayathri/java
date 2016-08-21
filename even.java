f73ab75807712070d340c4727284e3026c0b12e1
r-gayathri <rajigayathri1996@gmail.com>
2016-08-21 15:38:18 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class sd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		Scanner in=new Scanner(System.in);
+		n=in.nextInt();
+		if(n%2==0)
+			System.out.println("no is even");
+		else
+			System.out.println("no is odd");
+
+	}
+
+}
