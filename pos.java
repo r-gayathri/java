071e92d2315a43f34693fe7ea1edc8dc360d648a
r-gayathri <rajigayathri1996@gmail.com>
2016-08-21 15:41:25 +0530

    Add files via upload

---
+++ pos.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class pos {
+
+	public static void main(String[] args){
+int n;
+Scanner in=new Scanner(System.in);
+n=in.nextInt();
+if(n>0)
+	System.out.println("no is positive");
+else if(n<0)
+	System.out.println("no is negative");
+else
+	
+	System.out.println("no is zero");
+	
+	}
+
+}
