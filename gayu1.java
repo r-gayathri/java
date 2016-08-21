8184ed3d89cf70830f5868ee72d7fc5b7b553829
r-gayathri <rajigayathri1996@gmail.com>
2016-08-21 16:48:53 +0530

    Add files via upload

---
+++ gayu1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class gayu {
+
+	public static void main(String[] args)throws java.io.IOException {
+		Scanner in=new Scanner(System.in);
+		char ch;
+		ch=(char)System.in.read();
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+			System.out.println("given character is vowel");
+		else
+			System.out.println("given character is consonant");
+			
+		
+	}
+
+}
