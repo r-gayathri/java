e7f4eebd8470ec2f6a61fa7a29dd865b4225336a
r-gayathri <rajigayathri1996@gmail.com>
2016-08-21 16:33:55 +0530

    Add files via upload

---
+++ gayu.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+{
+public static void main(String[] arg)
+{
+ Scanner in=new Scanner (System.in);
+int N;
+N=in.nextInt();
+if(N%2==0)
+System.out.println("the given no is even");
+else
+
+System.out.println("the given no is odd");
+
+}
+
+}
