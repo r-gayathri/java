c89055ea365a921aa0317aecdd253085a828e11a
r-gayathri <rajigayathri1996@gmail.com>
2016-08-22 16:01:12 +0530

    Add files via upload

---
+++ strint.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class strint{
+
+	
+	public static void main(String[] args){
+		Scanner in=new Scanner(System.in);
+		String str="12345";
+		int a=Integer.parseInt(str);
+		
+		System.out.println(a);
+		
+		
+		
+	    
+	}
+
+}
