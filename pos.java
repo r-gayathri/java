8c219828d1baee2c1e7ee63f039bf06e61dc498d
r-gayathri <rajigayathri1996@gmail.com>
2016-08-22 11:28:35 +0530

    Add files via upload

--- pos.java
+++ pos.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
-public class pos {
+public class gayu {
 
-	public static void main(String[] args){
-int n;
-Scanner in=new Scanner(System.in);
-n=in.nextInt();
-if(n>0)
-	System.out.println("no is positive");
-else if(n<0)
-	System.out.println("no is negative");
-else
-	
-	System.out.println("no is zero");
-	
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int S;
+		S=in.nextInt();
+		if(S>0)
+			System.out.println("given no is positive");
+		if(S<0)
+			System.out.println("given no is negative");
+		
+		if(S==0)
+			System.out.println("given no is zero");
+			
+		// TODO Auto-generated method stub
+
 	}
 
 }
