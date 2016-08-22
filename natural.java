b2d3bcdee64908b10f69c25a7b6b9b04d1567b28
r-gayathri <rajigayathri1996@gmail.com>
2016-08-22 11:29:47 +0530

    Add files via upload

---
+++ natural.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+
+public class natural {
+
+	
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int a,sum=0;
+		a=in.nextInt();
+		for(int i=0;i<=a;i++)
+			sum=sum+i;
+	
+	System.out.println(sum);
+	}
+
+}
